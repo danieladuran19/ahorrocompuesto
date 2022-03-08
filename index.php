208a01ca2e7e916169aa6af73794900579ddfe7f
danieladuran19 <101167921+danieladuran19@users.noreply.github.com>
2022-03-08 00:36:22 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+    if(null !== $_GET['efectivo'] && null !==$_GET['tiempo'] && null !== $_GET['interes'] ){
+
+        $efectivo = floatval($_GET['efectivo']);
+        $tiempo = floatval($_GET['tiempo']);
+        $interes = floatval($_GET['interes']);
+        $monto_comp = $efectivo * pow((1+$interes), $tiempo);
+        $json = array("monto"=>$monto_comp);
+        header("HTTP/1.1 200 OK");
+        echo json_encode($json);
+    }
+
+}
+else {
+    header("HTTP/1.1 Bad Request");
+}
+
+?>
